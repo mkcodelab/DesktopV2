a916ef55bb9c754a7abc32ad9226eca04d791773
mikcodelab <michal.kulawik@gmail.com>
2024-03-05 12:22:50 +0100

    login component

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,21 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe, NgClass, NgIf } from '@angular/common';
+
+export interface UserInterface {
+  email: string,
+  username: string,
+  bio: string | null,
+  image: string,
+  token: string
+}
 
 @Component({
   standalone: true,
   selector: 'log-in',
-  template: `
-    <div class="flex">
-      <p class="text-lg mr-2">Log in component</p>
-      <p>{{ userName }}</p>
-      <p>logged in: {{ loggedIn }}</p>
-    </div>
-  `,
-  imports: [],
+  imports: [FormsModule, NgIf, NgClass, JsonPipe],
+  templateUrl: './log-in.component.html'
 })
 export class LogInComponent {
-  loggedIn = false;
-  userName = '';
-  logIn() {
-    console.log('user logged in');
+  private url = 'https://api.realworld.io/api/';
+
+  isLoggedIn = false;
+  username = '';
+  password = '';
+  email = '';
+
+  isRegisterActive = true
+
+  app = inject(AppComponent);
+
+  http = inject(HttpClient);
+
+  success = false
+
+  userResponseData: { user: UserInterface | undefined } = { user: undefined }
+
+  submit() {
+
+    if (this.isRegisterActive) {
+      this.register()
+    } else {
+      this.login()
+    }
+
+    if (this.success) {
+      this.closeModal();
+
+    }
+  }
+
+  closeModal() {
+    this.app.closeCentralModal('log-in-modal');
+  }
+
+  login() {
+    const payload = {
+      user: {
+        email: this.email,
+        password: this.password
+      }
+    }
+    this.http.post<{ user: UserInterface }>(this.url + 'users/login', payload).subscribe((response) => {
+      console.log(response);
+      this.userResponseData = response
+      this.isLoggedIn = true
+    });
+  }
+
+  register() {
+    const payload = {
+      user: {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }
+    }
+    this.closeModal();
+    this.http.post<{ user: UserInterface }>(this.url + 'users', payload).subscribe((response) => {
+      console.log(response);
+    });
   }
 }
